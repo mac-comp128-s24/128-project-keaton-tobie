e26c0a6aacede19af292f9fe36ef8d1b63d4c432
t-o-b-i-e <tschecte@macalester.edu>
2024-04-15 10:39:22 -0500

    board

--- src/Tile.java
+++ src/Tile.java
@@ -1,24 +1,10 @@
 public class Tile {
-    private Piece piece;
     private int row;
     private int col;
 
     public Tile(int row, int col) {
         this.row = row;
         this.col = col;
-        this.piece = null; // Initialize with no piece on the tile
-    }
-
-    public boolean isOccupied() {
-        return piece != null;
-    }
-
-    public Piece getPiece() {
-        return piece;
-    }
-
-    public void setPiece(Piece piece) {
-        this.piece = piece;
     }
 
     public int getRow() {
